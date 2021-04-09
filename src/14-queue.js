c7c866dc5bb5f1856ab33858817cdd90fe346b27
Elena Keda <elena_keda_92@mail.ru>
2021-04-09 18:49:21 +0300

    refactor: add solution 14(maybe finish))

--- src/14-queue.js
+++ src/14-queue.js
@@ -13,44 +13,30 @@ const ListNode = require('../extensions/list-node');
 
 class Queue {
   constructor() {
+    this.arr = [];
     this.front = null;
     this.back = null;
   }
 
   get size() {
-    const tempArr = [];
-    let temp = this.front;
-    while (temp) {
-      tempArr.push(temp.value);
-      temp = temp.next;
-    }
-    return tempArr.length;
-  }
-
-  isEmpty() {
-    return !this.front;
+    return this.arr.length;
   }
 
   enqueue(element) {
     const node = new ListNode(element);
-    if (this.isEmpty()) {
-      this.back = node;
-      this.front = this.back;
-    } else {
+    this.arr.push(element);
+    if (this.front) {
       this.back.next = node;
+      this.back = node;
+    } else {
+      this.front = node;
       this.back = node;
     }
   }
 
   dequeue() {
-    const node = this.front;
-    if (this.front) {
-      this.front = this.front.next;
-    }
-    if (!this.isEmpty()) {
-      this.back = null;
-    }
-    return node;
+    this.front = this.front.next;
+    return this.arr.shift();
   }
 }
 
