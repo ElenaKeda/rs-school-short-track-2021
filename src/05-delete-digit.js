5507c6b72f90d20f6bd969a2740520fc43082210
Elena Keda <elena_keda_92@mail.ru>
2021-04-09 14:39:26 +0300

    refactor: add solution 5

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,15 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  let arr = n.toString().split('');
+  const res = [];
+  for (let i = 0; i < arr.length; i++) {
+    arr.splice(i, 1);
+    res.push(arr);
+    arr = n.toString().split('');
+  }
+  return Math.max(...res.map((item) => item.join('')).map((item) => +item));
 }
 
 module.exports = deleteDigit;
