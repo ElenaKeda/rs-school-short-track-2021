253d0f4b88bb407826e78400ad1a1ae505c6087c
Elena Keda <elena_keda_92@mail.ru>
2021-04-09 16:05:34 +0300

    refactor: add solution 11(have a question to tests!!)

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,16 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const string = str;
+  const obj = {};
+  const chars = string.split('');
+  for (let i = 0; i < chars.length; i++) {
+    const count = obj[chars[i]] ? obj[chars[i]] : 0;
+    obj[chars[i]] = count + 1;
+  }
+  const s = Object.entries(obj).map((item) => (item.reverse())).flat().join('');
+  return s.replace(/1/gi, '');
 }
 
 module.exports = encodeLine;
