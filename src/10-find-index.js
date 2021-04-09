3b645544bedaf875aa2f3da23bf32f6421efbb79
Elena Keda <elena_keda_92@mail.ru>
2021-04-09 15:23:38 +0300

    refactor: add solution 10 (must fix!!!)

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,34 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(arr, val, startVal, endVal, i) {
+  let result;
+  let array = arr;
+  const value = val;
+  let start = startVal;
+  let end = endVal;
+  let ind = i;
+  end = array.length - 1;
+  start = start || 0;
+  ind = ind || 0;
+  const middle = Math.floor((start + (end - start) / 2));
+  if (value === array[middle]) {
+    // return middle;
+    result = middle;
+  }
+  if (end - 1 === start) {
+    result = Math.abs(array[start] - value) > Math.abs(array[end] - value) ? end : start;
+  }
+  if (value > array[middle]) {
+    ind = array.indexOf(array[middle]);
+    array = array.slice(middle);
+    result = findIndex(array, value, middle, end, ind) + ind;
+  }
+  if (value < array[middle]) {
+    array = array.slice(0, middle);
+    result = findIndex(array, value, start, middle, ind) + ind;
+  }
+  return result;
 }
 
 module.exports = findIndex;
